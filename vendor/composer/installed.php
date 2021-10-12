f45446296eb7ebbac6fe81f1e144f94c57cdca26
Ganesh Kandu <kanduganesh@gmail.com>
2021-10-12 16:43:00 +0530

    update
    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,42 +1,41 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'application',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'php-kchat/kchat',
+        'dev' => true,
     ),
-    'reference' => '988e663b75f3bb567905f88751d995b9355107f1',
-    'name' => 'php-kchat/kchat',
-  ),
-  'versions' => 
-  array (
-    'geoip/geoip' => 
-    array (
-      'pretty_version' => 'v1.17',
-      'version' => '1.17.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '2053a85c2ed3e7adcbaf0117e26832f57366e370',
+    'versions' => array(
+        'geoip/geoip' => array(
+            'pretty_version' => 'v1.17',
+            'version' => '1.17.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../geoip/geoip',
+            'aliases' => array(),
+            'reference' => '2053a85c2ed3e7adcbaf0117e26832f57366e370',
+            'dev_requirement' => false,
+        ),
+        'php-kchat/kchat' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'application',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.1',
+            'version' => '6.5.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+            'dev_requirement' => false,
+        ),
     ),
-    'php-kchat/kchat' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '988e663b75f3bb567905f88751d995b9355107f1',
-    ),
-    'phpmailer/phpmailer' => 
-    array (
-      'pretty_version' => 'v6.4.1',
-      'version' => '6.4.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '9256f12d8fb0cd0500f93b19e18c356906cbed3d',
-    ),
-  ),
 );
